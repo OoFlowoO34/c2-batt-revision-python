47fb634d9663545df49ad7fab3c1628b35f8cc58
florian <florian.batt@hotmail.fr>
2023-10-25 10:23:57 +0200

    exo3 Done

---
+++ exo/exo3.py
@@ -0,0 +1,10 @@
+print ("first number: ")
+firstNumber = int(input())
+
+print ("second number: ")
+secondNumber =  int(input())
+
+print("addition: ", firstNumber+secondNumber) 
+print("soustractions: ", firstNumber-secondNumber)
+print("multiplication: ", firstNumber*secondNumber)
+print("division: ", firstNumber/secondNumber)
