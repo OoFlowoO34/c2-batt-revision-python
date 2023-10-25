a149592997832258e5e0e697774c81634b672ed8
florian <florian.batt@hotmail.fr>
2023-10-25 12:31:59 +0200

    exo8, exo9 Done

---
+++ exo/exo8.py
@@ -0,0 +1,10 @@
+notes_des_eleves = {
+    "Alice": 90,
+    "Bob": 85,
+    "Charlie": 92,
+    "David": 88,
+    "Eve": 78
+}
+valeur_max = max(notes_des_eleves.values())
+
+print(valeur_max)
---
+++ exo/exo9.py
@@ -0,0 +1,2 @@
+carres = [n**2 for n in range(1, 11)]
+print(carres)
